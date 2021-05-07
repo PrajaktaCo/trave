924657a05c5e56784b9831e159cfda9b56368bf1
PrajaktaCo <prajakta.ohal@coditas.com>
2021-05-07 19:43:38 +0530

    Added Icon Button

---
+++ app/components/common/IconButton/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet , Dimensions} from 'react-native';
+import { COLORS, FONTS } from '../../../assets/style';
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        backgroundColor: COLORS.PRIMARY_COLOR, 
+        height: 40, 
+        width: 40, 
+        borderRadius: 20, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    }
+})
+
+export default styles;
