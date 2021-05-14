b4b8a1b9e5f5a7eb9a715095916fe55477360aa9
PrajaktaCo <prajakta.ohal@coditas.com>
2021-05-14 16:36:50 +0530

    Integrated navigation service

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { TabNavigation } from 'Navigation';
+import { navigationRef } from './app/services/NavigationService';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <TabNavigation />
     </NavigationContainer>
   );
